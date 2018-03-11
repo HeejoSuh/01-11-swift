a9b8ae27db1a14cdd7831eb2a75af672cdeb9765
HeejoSuh <15654639+HeejoSuh@users.noreply.github.com>
2018-03-11 00:17:04 -0500

    Update StudentInfoADT.swift

--- StudentInfoADT.swift
+++ StudentInfoADT.swift
@@ -14,107 +14,117 @@
 //  Created by Heejo Suh in Feb 2018
 //  Copyright @ 2018 MTHS. All rights reserved.
 //
-//			swiftc Planet.swift
+//			swiftc StudentInfoADT.swift
+
 
 //declare enum
 //declare data type
-enum GradesEnum : String {
-	TWELVE = "12",
-	ELEVEN = "11",
-	TEN = "10",
-	NINE = "9",
-	EIGHT = "8",
-	SEVEN = "7"
+enum GradesEnum: String {
+	case TWELVE="TWELVE"
+	case ELEVEN="ELEVEN"
+	case TEN="TEN"
+	case NINE="NINE"
+	case EIGHT="EIGHT"
+	case SEVEN="SEVEN"
 	
-	//
-	static let allGrades = [TWELVE, ELEVEN, TEN, NINE, EIGHT, SEVEN] //must maintain second copy of values
-	//to reference using index, use GradesEnum.allGrades[#]
 }
 
 
 //------------------------------------------------
+struct Student {
+	//ADT
+	
+	var _firstName: String
+	var _lastName: String
+	var _middleInitial: String
+	var _dateOfBirth: String //dd/mm/yyyy
+	//var _grade: Grades
+	var _grade: String
+	//var _identified: Boolean
+	var _identified: String
+
+	
+	func studentAllInfo()->String {
+		//returns full info
+		let fullInfo = "\(_firstName) \(_middleInitial) \(_lastName) in grade \(_grade) "
+		return fullInfo
+	}
+}
 
 
-firstName = "First name"
-lastName = "Last name"
-dateOfBirth = "Date of Birth"
-middleInitial = "Middle Initial"
-grade = "Grade"
-identified = "if the student is identified"
+
+//------------------------------------------------
+
+var firstName = "first name"
+var lastName = "last name"
+var dateOfBirth = "date of Birth"
+var middleInitial = "middle Initial"
+var grade = "grade"
+var identified = "if the student is identified"
 		
-var currentStudent: [String] = [firstName, lastName, middleInitial, dateOfBirth, grade, identified]
+		
+var currentStudent: [String]
 var currentInput: String?
 
+		
+var classRoomStudentCount = 3
+var classRoom: [Student] = []
+var nthStudent = 0
+var notCorrectInput = true
 
-while true{
+while nthStudent<classRoomStudentCount {
 //get each input
+	currentStudent = [firstName, lastName, middleInitial, dateOfBirth, grade, identified]
 	for index in 0..<currentStudent.count {
-		//for each assignment
-		getCorrectInput: while true{
-			print("\nEnter \(currentStudent[index]) for student")
+		//for each info
+		notCorrectInput = true
+		while notCorrectInput==true{
+			print("\nEnter \(currentStudent[index]) for student #\(nthStudent+1)")
 			//print options for grades
+			let allGrades = ["TWELVE", "ELEVEN", "TEN", "NINE", "EIGHT", "SEVEN"]
 			//---------------------------
 			if currentStudent[index] == "Grade" {
 				print("Options: ")
 				//for grades, print possible inputs
-				//for(GradesEnum grades : GradesEnum.values()) { 
+				//for(GradesEnum grades : GradesEnum.values()) 
 				print(allGrades)
-			}				
+			}			
 							
-			input = readLine(strippingNewline: true)
-			}
+			currentInput = readLine(strippingNewline: true)
 
 			//check if valid
 			//---------------------------
 			if currentStudent[index] == grade{
-				if let currentGrade = PlanetEnum(rawValue: (currentInput!).uppercased()){
+				if GradesEnum(rawValue: currentInput!.uppercased()) != nil {
+				//check by string
 					//if input is in enum, add to student
-					currentStudent[index] = String(currentGrade)
-					break getCorrectInput //get next input
-				}else{
+					currentStudent[index] = currentInput!
+					notCorrectInput = false //get next input
+				} else {
 					//grade not in enum
 					print("Please input a valid grade!\n")
 				}
 			//---------------------------
-			}else if currentStudent[index] == identified{
+			} else if currentStudent[index] == identified{
 				//if input is yes or no
 				//compare **string**
 				if (currentInput=="y"||currentInput=="Y"||currentInput=="n"||currentInput=="N") {
-					currentStudent[index] = currentInput //add to list
-					break getCorrectInput
-				}else {
+					currentStudent[index] = currentInput! //add to list
+					notCorrectInput = false
+				} else {
 					print("Please enter  Y or N for yes or no!\n")
 				}
 			//---------------------------
-			}else{
+			} else {
 				//no specific type required
-				currentStudent[index] = currentInput //add to list
-				break getCorrectInput
+				currentStudent[index] = currentInput! //add to list
+				notCorrectInput = false
 			}
 			//---------------------------
 		}
 	}
+	classRoom.append(Student(_firstName: currentStudent[0], _lastName: currentStudent[1], _middleInitial: currentStudent[2], _dateOfBirth: currentStudent[3], _grade: currentStudent[4], _identified:currentStudent[5]))
+	print("Student #\(nthStudent+1): \(classRoom[nthStudent].studentAllInfo())")
 	
-	var thisStudent = Student((currentStudent[0], currentStudent[1], currentStudent[2], currentStudent[3], currentStudent[4], currentStudent[5]))
-}
-			
-	
-}
-
-
-public struct Student{
-	public string firstName;
-	public string lastName;
-	public char middleInitial;
-	public byte grade;
-	public bool identified;
-	
-	
-	public string FullName(){
-	// this function returns the full name
-	// first, Initial and Last of a student
-		string fullNameOfStudent;
-		fullNameOfStudent = this.firstName + " " + this.middleInitial+ " " + this.lastName;
-		return fullNameOfStudent;
-	}
+	nthStudent+=1
 }
